992032d770b180f79686f444b937813408a73db7
Sullyfrank <willsheehey@gmail.com>
2025-05-14 12:19:45 -0700

    project 5.1 forEach

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,33 @@
+const initialCards = [
+
+  {
+  name: "Val Thorens",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+    },
+  {
+  name: "Restaurant terrace",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
+  },
+ {
+  name: "An outdoor cafe",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
+  },
+ {
+  name: "A very long bridge, over the forest and through the trees",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
+  },
+{
+  name: "Tunnel with morning light",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
+  },
+
+ {
+  name: "Mountain house",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
+  },
+
+  ];
+
 const editProfileButton = document.querySelector(".profile__button")
 
 const editProfileModal = document.querySelector("#edit-profile-modal")
@@ -65,3 +95,10 @@ function handlePostFormSubmit(evt) {
 
 
 newPostFrom.addEventListener('submit', handlePostFormSubmit);
+
+initialCards.forEach(function(item)
+{
+  console.log(item.name)
+  console.log(item.link)
+}
+);
